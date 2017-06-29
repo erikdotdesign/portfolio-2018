afc7a2da78dd4376d3f421d75719f7407f55334a
Erik Myers <erikmyers@eriks-mbp.attlocal.net>
2017-06-29 13:55:07 -0700

    Updated styles structure and added semicolons to js lint

--- src/routes/Counter/components/Counter.js
+++ src/routes/Counter/components/Counter.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from 'react';
+import PropTypes from 'prop-types';
 
 export const Counter = ({ counter, increment, doubleAsync }) => (
   <div style={{ margin: '0 auto' }} >
@@ -12,11 +12,11 @@ export const Counter = ({ counter, increment, doubleAsync }) => (
       Double (Async)
     </button>
   </div>
-)
+);
 Counter.propTypes = {
   counter: PropTypes.number.isRequired,
   increment: PropTypes.func.isRequired,
   doubleAsync: PropTypes.func.isRequired,
-}
+};
 
-export default Counter
+export default Counter;
