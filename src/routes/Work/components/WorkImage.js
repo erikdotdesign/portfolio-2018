cf95eb265bbedada92f3372c1473d40f00049c4e
Erik Myers <erikmyers@eriks-mbp.attlocal.net>
2017-08-07 18:00:26 -0700

    added aspect to work images

--- src/routes/Work/components/WorkImage.js
+++ src/routes/Work/components/WorkImage.js
@@ -11,9 +11,12 @@ class WorkImage extends React.Component {
         {({isVisible}) =>
           <div className="c-work-image">
             <HalftoneCharacter className="c-work-item__index" character={this.props.index + 1} />
-            <BlockReveal blockColor={this.props.hex} forcePlay={isVisible} inline={false} delay={0} duration={0.5} direction={this.props.index % 2 == 0 ? "leftRight" : "rightLeft"}>
-              <img src={isVisible ? this.props.url : null} alt={this.props.alt} />
-            </BlockReveal>
+            <div className="c-work-image__aspect">
+              <BlockReveal blockColor={this.props.hex} forcePlay={isVisible} inline={false} delay={0} duration={0.5} direction={this.props.index % 2 == 0 ? "leftRight" : "rightLeft"}>
+                <img src={isVisible ? this.props.url : null} alt={this.props.alt} />
+              </BlockReveal>
+              <div className="c-work-image__bg"></div>
+            </div>
             <BlockReveal blockColor={this.props.hex} forcePlay={isVisible} inline={false} delay={0} duration={0.5} direction={this.props.index % 2 == 0 ? "rightLeft" : "leftRight"}>
               <p className="caption">{this.props.alt}</p>
             </BlockReveal>
