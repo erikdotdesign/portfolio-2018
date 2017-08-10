1c5c2b3ae150dbaefb792cfb1941342ba93bd575
Erik Myers <erikmyers@eriks-mbp.attlocal.net>
2017-08-10 16:03:57 -0700

    small button update

--- src/components/Button.js
+++ src/components/Button.js
@@ -14,11 +14,7 @@ class Button extends React.Component {
   }
   hexToRgba = (hex, alpha) => {
     var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-    return result
-           ?
-           `rgba(${parseInt(result[1], 16)}, ${parseInt(result[2], 16)}, ${parseInt(result[3], 16)}, ${alpha})`
-           :
-           null;
+    return `rgba(${parseInt(result[1], 16)}, ${parseInt(result[2], 16)}, ${parseInt(result[3], 16)}, ${alpha})`;
   }
   render () {
     let linkStyle = this.state.hover ? {background: this.hexToRgba(this.props.hex, 0.2)} : {background: 'none'};
