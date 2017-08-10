d3aee81c511f33c0c95f44966571d33f2ee63912
Erik Myers <erikmyers@eriks-mbp.attlocal.net>
2017-08-10 16:41:44 -0700

    hero formatting

--- src/routes/Home/components/HomeHero.js
+++ src/routes/Home/components/HomeHero.js
@@ -11,10 +11,21 @@ class HomeHero extends React.Component {
         {({ isVisible }) =>
           <Hero rightHalftoneText='17/18'>
             <div className='single single-measure'>
-              <BlockReveal inline={false} forcePlay={isVisible} delay={0} textShift duration={0.5}>
+              <BlockReveal
+              inline={false}
+              forcePlay={isVisible}
+              delay={0}
+              textShift
+              duration={0.5}>
                 <h1 className='epsilon--display-condensed'>Erik Myers</h1>
               </BlockReveal>
-              <BlockReveal inline={false} forcePlay={isVisible} delay={0.2} textShift duration={0.5} direction='rightLeft'>
+              <BlockReveal
+              inline={false}
+              forcePlay={isVisible}
+              delay={0.2}
+              textShift
+              duration={0.5}
+              direction='rightLeft'>
                 <h1 className='epsilon--display-condensed'>Design</h1>
               </BlockReveal>
             </div>
