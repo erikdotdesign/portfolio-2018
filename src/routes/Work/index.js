6f056724ebbeb81f38ba6260dcec9b4bf1526110
Erik Myers <erikmyers@eriks-mbp.attlocal.net>
2017-08-14 17:40:15 -0700

    update to routing

--- src/routes/Work/index.js
+++ src/routes/Work/index.js
@@ -1,6 +1,23 @@
-import WorkView from './components/WorkView';
-
+// Sync route definition
 export default {
   path : 'work/:workId',
-  component : WorkView
+  /*  Async getComponent is only invoked when route matches   */
+  getComponent (nextState, cb) {
+    /*  Webpack - use 'require.ensure' to create a split point
+        and embed an async module loader (jsonp) when bundling   */
+    require.ensure([], (require) => {
+      /*  Webpack - use require callback to define
+          dependencies for bundling   */
+      const WorkView = require('./components/WorkView').default;
+      //const reducer = require('./modules/counter').default;
+
+      /*  Add the reducer to the store on key 'counter'  */
+      //injectReducer(store, { key: 'counter', reducer });
+
+      /*  Return getComponent   */
+      cb(null, WorkView);
+
+    /* Webpack named bundle   */
+    }, 'work');
+  }
 };
