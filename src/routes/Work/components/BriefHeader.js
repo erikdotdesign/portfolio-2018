1c30ef9e982998cc9558a514189661e5126e140a
Erik Myers <erikmyers@eriks-mbp.attlocal.net>
2017-09-14 10:31:19 -0700

    removed unneeded div

--- src/routes/Work/components/BriefHeader.js
+++ src/routes/Work/components/BriefHeader.js
@@ -3,20 +3,18 @@ import PropTypes from 'prop-types';
 import BlockReveal from '../../../components/BlockReveal';
 
 const BriefHeader = ({ forcePlay, hex, blockId }) => (
-  <div>
-    <h3 className='top-stroke--strong top-stroke--inline'>
-      <BlockReveal
-        blockColor={hex}
-        forcePlay={forcePlay}
-        blockId={blockId}
-        inline={false}
-        delay={0}
-        duration={0.5}
-        direction='rightLeft'>
-        Brief
-      </BlockReveal>
-    </h3>
-  </div>
+  <h3 className='top-stroke--strong top-stroke--inline'>
+    <BlockReveal
+      blockColor={hex}
+      forcePlay={forcePlay}
+      blockId={blockId}
+      inline={false}
+      delay={0}
+      duration={0.5}
+      direction='rightLeft'>
+      Brief
+    </BlockReveal>
+  </h3>
 );
 
 BriefHeader.propTypes = {
