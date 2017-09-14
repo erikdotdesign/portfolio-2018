09ea11171c48efc687e3758ebd73a40e2ed9ad26
Erik Myers <erikmyers@eriks-mbp.attlocal.net>
2017-09-14 10:18:30 -0700

    removed textShift prop

--- src/components/BlockReveal.js
+++ src/components/BlockReveal.js
@@ -5,7 +5,7 @@ import VisibilitySensor from 'react-visibility-sensor';
 import GSAP from 'react-gsap-enhancer';
 
 function createAnim ({ options }) {
-  const { direction, delay, duration, textShift, blockColor } = options.props;
+  const { direction, delay, duration, blockColor } = options.props;
   const { element, block } = options.refs;
   const defaultBlockColor = 'yellow';
 
@@ -17,83 +17,94 @@ function createAnim ({ options }) {
       .set(block, {
         transformOrigin: '0 50%',
         background: blockColor || defaultBlockColor,
-        left: textShift ? -5 : 0,
-        width: `calc(100% + ${textShift ? '5px' : '0px'})` }, `+=${delay}`)
+      }, `+=${delay}`)
       .from(block, duration, {
         transform: 'scale3d(0,1,1)',
-        ease: Quint.easeInOut })
-      .set(element, { opacity: 1 })
-      .from(element, duration, { x: textShift ? -5 : 0 })
+        ease: Quint.easeInOut
+      })
+      .set(element, {
+        opacity: 1
+      })
       .to(block, duration, {
         transform: 'scale3d(0,1,1)',
         transformOrigin: '100% 50%',
-        ease: Quint.easeInOut }, `-=${duration}`);
+        ease: Quint.easeInOut
+      });
       break;
     case 'rightLeft':
       blockTimeline
       .set(block, {
         transformOrigin: '100% 50%',
         background: blockColor || defaultBlockColor,
-        right: textShift ? -5 : 0,
-        width: `calc(100% + ${textShift ? '5px' : '0px'})` }, `+=${delay}`)
+      }, `+=${delay}`)
       .from(block, duration, {
         transform: 'scale3d(0,1,1)',
-        ease: Quint.easeInOut })
-      .set(element, { opacity: 1 })
-      .from(element, duration, { x: textShift ? 5 : 0 })
+        ease: Quint.easeInOut
+      })
+      .set(element, {
+        opacity: 1
+      })
       .to(block, duration, {
         transform: 'scale3d(0,1,1)',
         transformOrigin: '0 50%',
-        ease: Quint.easeInOut }, `-=${duration}`);
+        ease: Quint.easeInOut
+      });
       break;
     case 'topBottom':
       blockTimeline
       .set(block, {
         transformOrigin: '50% 0',
         background: blockColor || defaultBlockColor,
-        y: textShift ? -5 : 0,
-        height: `calc(100% + ${textShift ? '5px' : '0px'})` }, `+=${delay}`)
+      }, `+=${delay}`)
       .from(block, duration, {
         transform: 'scale3d(1,0,1)',
         ease: Quint.easeInOut })
-      .set(element, { opacity: 1 })
-      .from(element, duration, { y: textShift ? -5 : 0 })
+      .set(element, {
+        opacity: 1
+      })
       .to(block, duration, {
         transform: 'scale3d(1,0,1)',
         transformOrigin: '50% 100%',
-        ease: Quint.easeInOut }, `-=${duration}`);
+        ease: Quint.easeInOut
+      });
       break;
     case 'bottomTop':
       blockTimeline
       .set(block, {
         transformOrigin: '50% 100%',
         background: blockColor || defaultBlockColor,
-        y: textShift ? -5 : 0,
-        height: `calc(100% + ${textShift ? '5px' : '0px'})` }, `+=${delay}`)
-      .from(block, duration, { transform: 'scale3d(1,0,1)', ease: Quint.easeInOut })
-      .set(element, { opacity: 1 })
-      .from(element, duration, { y: textShift ? 5 : 0 })
+      }, `+=${delay}`)
+      .from(block, duration, {
+        transform: 'scale3d(1,0,1)',
+        ease: Quint.easeInOut
+      })
+      .set(element, {
+        opacity: 1
+      })
       .to(block, duration, {
         transform: 'scale3d(1,0,1)',
         transformOrigin: '50% 0',
-        ease: Quint.easeInOut }, `-=${duration}`);
+        ease: Quint.easeInOut
+      });
       break;
     default:
       blockTimeline
       .set(block, {
         transformOrigin: '0 50%',
-        background: blockColor || defaultBlockColor,
-        left: textShift ? -5 : 0,
-        width: `calc(100% + ${textShift ? '5px' : '0px'})` }, `+=${delay}`)
+        background: blockColor || defaultBlockColor
+      }, `+=${delay}`)
       .from(block, duration, {
         transform: 'scale3d(0,1,1)',
-        ease: Quint.easeInOut })
-      .set(element, { opacity: 1 })
-      .from(element, duration, { x: textShift ? -5 : 0 })
+        ease: Quint.easeInOut
+      })
+      .set(element, {
+        opacity: 1
+      })
       .to(block, duration, {
         transform: 'scale3d(0,1,1)',
         transformOrigin: '100% 50%',
-        ease: Quint.easeInOut }, `-=${duration}`);
+        ease: Quint.easeInOut
+      });
   }
 
   return blockTimeline;
@@ -172,7 +183,6 @@ BlockReveal.propTypes = {
   delay: PropTypes.number.isRequired,
   blockId: PropTypes.string,
   duration: PropTypes.number.isRequired,
-  textShift: PropTypes.bool,
   background: PropTypes.string,
   forcePlay: PropTypes.bool
 };
