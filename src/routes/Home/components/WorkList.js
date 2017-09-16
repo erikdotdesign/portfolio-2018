10f04d79a0138fe7edc7a02ca04e1bff47a82d02
Erik Myers <erikmyers@eriks-mbp.attlocal.net>
2017-09-16 12:30:05 -0700

    removed unused sass file

--- src/routes/Home/components/WorkList.js
+++ src/routes/Home/components/WorkList.js
@@ -5,7 +5,7 @@ const WorkList = ({ workList }) => (
   <main className='c-work-list' role='main'>
     {
       workList.map((workItem, index) =>
-        <WorkItem work={workItem} index={index} key={index} />
+        <WorkItem work={workItem} index={index} halftoneText={index + 1} key={index} />
       )
     }
   </main>
