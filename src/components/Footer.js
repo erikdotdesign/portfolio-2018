633a4e62fcdc81b8deecb510e5b06296993b572d
Erik Myers <erikmyers@eriks-mbp.attlocal.net>
2017-09-16 13:35:10 -0700

    added footer

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = () => (
+  <footer className='c-footer section' role='contentinfo'>
+    <div className='columns-flush'>
+      <div className='column column--duo'>
+        <p>&copy; Erik Myers Design 2017</p>
+        <p className='caption'>Created with React, Redux, TimelineMax</p>
+      </div>
+    </div>
+  </footer>
+);
+
+export default Footer;
--- src/layouts/PageLayout/PageLayout.js
+++ src/layouts/PageLayout/PageLayout.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import TopNav from '../../components/TopNav';
+import Footer from '../../components/Footer';
 
 const PageLayout = ({ children }) => (
   <div className='c-page-layout'>
     <TopNav />
     {children}
+    <Footer />
   </div>
 );
 
