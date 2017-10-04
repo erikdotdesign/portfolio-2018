c1e31bb1ae7c9f30d772518774aa314f574a28c3
Erik Myers <erikmyers@eriks-mbp.attlocal.net>
2017-10-04 12:58:35 -0700

    updated footer with new styles and links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import OutlineButton from './OutlineButton';
 
 const Footer = () => (
   <footer className='c-footer section' role='contentinfo'>
     <div className='columns-flush'>
       <div className='column column--duo'>
-        <p>&copy; Erik Myers Design 2017</p>
-        <p className='caption'>Created with React, Redux, GSAP</p>
+        <p>&copy; Erik Myers Design 2018</p>
+        <a
+          className='c-footer__link'
+          title='Github Repo'
+          target='_blank'
+          href='https://github.com/endswithak/portfolio-2018'>
+          Created with React, Redux, GSAP
+        </a>
       </div>
     </div>
   </footer>
