e2e0ad214dc92fb8980a86dfd67092b05a84575b
Erik <erik@Eriks-MacBook-Pro.local>
2019-10-01 18:02:33 -0400

    updated caffeine about brief

--- src/store/data/caffeine.about.js
+++ src/store/data/caffeine.about.js
@@ -5,11 +5,9 @@ const caffeineAbout = {
   'year': '2018',
   'snippet': `About page and signup flow redesign for Caffeine's web and iOS applications.`,
   'description': [
-    `The objective for this project was to redesign Caffeine’s about page and signup flow.
-    The goals for this project were to emphasize core product features, underscore available
-    platforms, feature partnered broadcasters, and have a fun & inviting design language.
-    Both items were designed in tandem. The about page took the form of a fully responsive
-    webpage while the signup flow had to be catered to Caffeine’s web and iOS applications.`
+    `The objective for this project was to redesign Caffeine’s About Page and Sign Up Flow. The goals were to emphasize core product features (e.g. chat and player experience for viewers and broadcasters), underscore available platforms (e.g. web and iOS), spotlight partnered broadcasters, and create a friendly design language. Both items were designed in tandem for a cohesive product experience. The About Page took the form of a fully responsive webpage while the Sign Up flow was created for Caffeine’s web and iOS applications.`,
+    `The prevailing theme for the Sign Up flow was making friends around Caffeine’s diverse content catalog. It was centered around three main areas: Entertainment, Gaming, and Creative Arts. Both the web and iOS experience featured a tagline that rotated between each area (e.g. Sign up and make friends around live entertainment.). For the web experience, each section also included relevant platform visuals with corresponding partnered broadcasters.`,
+    `The goal for the About Page was to welcome new users unfamiliar with the product, brief them on Caffeine, and compel them to sign up. It was broken into six sections. The first section included an introductory video along with a welcome statement. The next three sections dove into what made Caffeine unique compared to its competitors. And the final two sections provided users with Sign Up and Contact links. Included with each section were relevant illustrations and platform visuals.`
   ],
   'links': null,
   'mobile': null,
