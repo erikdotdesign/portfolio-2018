526d954ee80a2b89764919234121b86ddafcd063
Erik <erik@Eriks-MacBook-Pro.local>
2019-10-03 11:53:07 -0400

    added live host app store screen and minor copy updates to live host and docmatter

--- src/store/data/caffeine.live-host.js
+++ src/store/data/caffeine.live-host.js
@@ -24,31 +24,35 @@ const caffeineLiveHost = {
   'images': [{
     'url': require('../../routes/WorkDetail/assets/img/lh-g-1.png'),
     'retina': require('../../routes/WorkDetail/assets/img/lh-g-1@2x.png'),
-    'alt': 'Default broadcaster interface (web on the left and iOS on the right). Clicking the tv icon will prompt the live host selection menu.'
+    'alt': 'Default broadcaster interface (web on the left and iOS on the right): Clicking the tv icon will prompt the live host selection menu.'
   }, {
     'url': require('../../routes/WorkDetail/assets/img/lh-g-2.png'),
     'retina': require('../../routes/WorkDetail/assets/img/lh-g-2@2x.png'),
-    'alt': 'Live host selection menu. From the live host selection menu, broadcasters can select the broadcast they’d like to host, change hosts, or view upcoming content with the programming guide.'
+    'alt': 'Live host selection menu: From the live host selection menu, broadcasters can select the broadcast they’d like to host, change hosts, or view upcoming content with the programming guide.'
   }, {
     'url': require('../../routes/WorkDetail/assets/img/lh-g-3.png'),
     'retina': require('../../routes/WorkDetail/assets/img/lh-g-3@2x.png'),
-    'alt': 'Programming guide. The programming guide provides broadcasters with a list of upcoming hostable content.'
+    'alt': 'Programming guide: The programming guide provides broadcasters with a list of upcoming hostable content.'
   }, {
     'url': require('../../routes/WorkDetail/assets/img/lh-g-4.png'),
     'retina': require('../../routes/WorkDetail/assets/img/lh-g-4@2x.png'),
-    'alt': 'Hosting interface. After selecting a broadcast to host, the broadcasters’ webcam shrinks and the hosted broadcast is appended to the player.'
+    'alt': 'Hosting interface: After selecting a broadcast to host, the broadcasters’ webcam shrinks and the hosted broadcast is appended to the player.'
   }, {
     'url': require('../../routes/WorkDetail/assets/img/lh-g-5.png'),
     'retina': require('../../routes/WorkDetail/assets/img/lh-g-5@2x.png'),
-    'alt': 'Live interface. Once live, the player controls move out of frame to give the broadcaster a better viewing experience. The controls can be toggled by hovering over the player on web and tapping the player on iOS.'
+    'alt': 'Live interface: Once live, the player controls move out of frame to give the broadcaster a better viewing experience. The controls can be toggled by hovering over the player on web and tapping the player on iOS.'
   }, {
     'url': require('../../routes/WorkDetail/assets/img/lh-g-6.png'),
     'retina': require('../../routes/WorkDetail/assets/img/lh-g-6@2x.png'),
-    'alt': 'Switching hosts. Broadcasters can seamlessly add, change, or remove a host even when they’re live.'
+    'alt': 'Switching hosts: Broadcasters can seamlessly add, change, or remove a host even when they’re live.'
   }, {
     'url': require('../../routes/WorkDetail/assets/img/lh-g-7.png'),
     'retina': require('../../routes/WorkDetail/assets/img/lh-g-7@2x.png'),
-    'alt': 'Host controls. Broadcasters can also mute or granularly change the playback volume of the hosted broadcast.'
+    'alt': 'Host controls: Broadcasters can also mute or granularly change the playback volume of the hosted broadcast.'
+  }, {
+    'url': require('../../routes/WorkDetail/assets/img/lh-g-8.png'),
+    'retina': require('../../routes/WorkDetail/assets/img/lh-g-8@2x.png'),
+    'alt': 'App Store images.'
   }]
 };
 
--- src/store/data/docmatter.js
+++ src/store/data/docmatter.js
@@ -14,7 +14,7 @@ const docmatter = {
     bring their ideas to life. Branding is that split-second feeling you have
     about a product. It’s not just a logo and color palette; It’s a system of
     experiences created through smart design and strategy. For DocMatter, the
-    strategy was simple. It had to clean and professional.`,
+    strategy was simple. It had to be clean and professional.`,
     `The color palette is a combination of dark gray, white, and dark red.
     The dark gray hue acts as the primary and the dark red acts as the accent.
     The dark gray gives an impression of elegance and modernism, while the dark
@@ -27,7 +27,7 @@ const docmatter = {
     symbol. The “Doc” is stacked on top of the “Matter” in a way that creates
     a doctor icon with the “o” of “Doc” and “M” of “Matter”. The medical cross
     acts as an accent to help tie it all together. The doctor icon may not be
-    apparent at first glance, but thats by design. It works similar to the arrow
+    apparent at first glance, but that's by design. It works similar to the arrow
     in the FedEx logo. If you’re thinking “But Erik, there is no arrow in the
     FedEx logo!”; I can assure you there is. Just google it and wait for your
     mind to be blown. It’s through these moments of discovery that make
